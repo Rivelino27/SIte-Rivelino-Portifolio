29701242d08a7477ce902669c0e526d7a54bbb6b
R Soares <r22777@outlook.com>
2022-12-22 04:37:51 -0300

    1

--- jsr27.js
+++ jsr27.js
@@ -17,3 +17,20 @@ function prev(){
 }
 
 setInterval(next, time);
+
+function makeQuote() {
+
+    Q = new Array();
+    
+    Q[0] = "<i>Assim como o sol derrete o gelo, a gentileza evapora mal-entendidos, desconfianças e hostilidade.</i>Albert Schweitzer"
+    
+    Q[1] = "<i>É impossível fazer qualquer coisa à prova de tolos, porque os tolos são muito engenhosos.</i>Arthur Bloch"
+    
+    Q[2] = "<i>Quem ama crê no impossível.</i> Elisabeth B. Browning"
+    
+    index = Math.floor(Math.random() * Q.length);
+    
+    /* document.write.innerHTML(Q[index])[0]; */
+    const list = document.getElementById("q7");
+    list.innerHTML = `<p>${Q[index]}</p>`;
+    }
